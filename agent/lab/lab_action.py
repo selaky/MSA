765015f16b431799977401d2e0e42f3307b31506
selaky <59405175+selaky@users.noreply.github.com>
2026-01-30 19:04:23 +0800

    修复返回类型不一致的问题

--- agent/lab/lab_action.py
+++ agent/lab/lab_action.py
@@ -9,6 +9,6 @@ from utils import common_func
 @AgentServer.custom_action("select_all_low_star")
 class SelectAllLowStar(CustomAction):
     """依次点击所有一二三星卡"""
-    def run(self,context:Context,argv:CustomAction.RunArg) -> bool:
-        common_func.group_click(context,lab_manager.batch_select_rois)
+    def run(self, context: Context, argv: CustomAction.RunArg) -> CustomAction.RunResult:
+        common_func.group_click(context, lab_manager.batch_select_rois)
         return CustomAction.RunResult(success=True)
